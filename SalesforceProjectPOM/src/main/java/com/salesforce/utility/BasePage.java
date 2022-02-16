69fc7a6b08d45d5d855b764ddc0d9c133046a3f9
Eepsa Padhi <eepsa.padhi09@gmail.com>
2022-02-15 21:10:33 -0800

    3

--- SalesforceProjectPOM/src/main/java/com/salesforce/utility/BasePage.java
+++ SalesforceProjectPOM/src/main/java/com/salesforce/utility/BasePage.java
@@ -1,39 +1,14 @@
 package com.salesforce.utility;
 
-import java.io.File;
-
-import java.io.IOException;
-import java.lang.reflect.Method;
-import java.util.concurrent.TimeUnit;
-
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
-import org.testng.ITestResult;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
-
-import com.salesforce.utility.Commmonutilities;
-import com.salesforce.utility.Constant;
-import com.salesforce.utility.GenerateReports;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BasePage {
 	protected static WebDriver driver;
